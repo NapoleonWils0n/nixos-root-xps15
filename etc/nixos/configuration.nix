1afcb179c7116262dbdf9c61a5e4796bb65bcbdf
Daniel J Wilcox <danieljwilcox@gmail.com>
2023-04-03 15:24:19 +0100

    nixos qemu run as root false

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -55,6 +55,7 @@
     libvirtd = {
       enable = true;
       qemu = {
+        runAsRoot = false;
         swtpm.enable = true;
         ovmf.enable = true;
         ovmf.packages = [ pkgs.OVMFFull.fd ];
